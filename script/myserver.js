50d7b77074a16b1f7403c47e8d4e890ad58d7979
lucas <lucassidpaula@gmail.com>
2024-09-12 18:07:07 -0300

    docs(dependencias): aplicação do monitoriamento do servidor e sua inicialização

--- script/myserver.js
+++ script/myserver.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mysql = require("mysql2/promise");
 
 app.listen(port, () => {
   console.log("meu servidor está levantao na porta", port);
