37a8e51525fc2d1a4953da046ec8cb34d5a33002
lucas <lucassidpaula@gmail.com>
2024-09-18 12:08:10 -0300

    feat(User):criando uma camada de segurança ao adicionar o usuário no banco de dados

--- script/controllers/User.js
+++ script/controllers/User.js
@@ -1,20 +1,31 @@
 const { User } = require("../models/User");
 const bcrypt = require("bcrypt");
+const db = require("../connectionDb");
 
 module.exports = {
   CreateUser: async (req, response) => {
     try {
       const { name, email, password } = req.body;
+      const query = `SELECT * FROM users WHERE email = '${email}';`;
+      const [retornoDaQuery] = await db.query(query);
+
+      console.log(retornoDaQuery);
+      if (retornoDaQuery.length != 0) {
+        console.log("está entrando aqui");
+        throw new Error("usuário ja cadastrado");
+      }
 
       const CriarUser = await new User(name, email, password).CreateUser();
       console.log("ola planeta");
 
       response.json(CriarUser);
     } catch (error) {
-      response.send(error);
+      console.log("não ta chetgando aqui");
+
+      response.send(error.message);
     }
   },
-  DeleteUser: () => {
+  DeleteUser: async (req, response) => {
     console.log("irei deleter o usuário");
   },
 };
